062758c43a859aa3a58e7b238068f69a47962437
iyedbenmansour <tniyed@gmail.com>
2025-03-01 15:58:31 +0100

    some design update

---
+++ src/main/java/org/example/auth/controllers/Congee/ChatbotController.java
@@ -0,0 +1,15 @@
+package org.example.auth.controllers.Congee;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ChatbotController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
---
+++ src/main/java/org/example/auth/controllers/Congee/EmployeeRankingsController.java
@@ -0,0 +1,4 @@
+package org.example.auth.controllers.Congee;
+
+public class EmployeeRankingsController {
+}
